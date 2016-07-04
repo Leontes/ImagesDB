743bc3de0e8f802cb34313e59165fc536ace9e91
Leontes <theshaljas@gmail.com>
2016-07-04 18:35:10 +0200

    Models creados

--- ImagesDBApp/models.py
+++ ImagesDBApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+	email = models.EmailField()
+	nombre = models.CharField(blank=True, null=True, max_length = 25)
+	apellidos = models.CharField(blank=True, null=True, max_length = 50)
+	fecha_registro = models.DateTimeField(auto_now_add=True, auto_now=False)
+	actualizado = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+	def __unicode__(self):
+		return self.nombre
+
+class Imagen(models.Model):
+    dueño = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    url_img = models.URLField()
+
+    def __unicode__(self):
+        return self.dueño.nombre
--- ImagesDBApp/views.py
+++ ImagesDBApp/views.py
@@ -1,3 +1,47 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
+from .forms import RegistroForm, ContactoForm
+from .models import Registro
+
 
 # Create your views here.
+def base(request):
+	return render(request, "base.html")
+
+#Navbar
+def sobre(request):
+	return render(request, "sobre.html", {})
+
+def home(request):
+	titulo = 'Bienvenido'
+
+	#Declaracion form
+	form =  RegistroForm(request.POST or None)
+
+	contexto = {
+		"template_title": titulo,
+		"a": 123,
+		"form": form
+	}
+
+	if form.is_valid():
+		instancia = form.save(commit=False)
+
+		nombre = form.cleaned_data.get("nombre")
+		if not nombre:
+			nombre = "Nuevo nombre"
+		instancia.nombre = nombre
+
+		instancia.save()
+		contexto = {
+			"template_title": "Gracias"
+		}
+
+	if request.user.is_authenticated() and request.user.is_staff:
+		queryset = Registro.objects.all().order_by('-fecha_registro')
+		contexto = {
+			"queryset": queryset
+		}
+
+	return render(request, "home.html",contexto)
