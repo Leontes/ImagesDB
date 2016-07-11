b9787929c50259cd77eeb01db71664d6f4d0f908
Leontes <shaljas@correo.ugr.es>
2016-07-11 20:02:58 +0200

    Despliegue heroku

--- ImagesDB/settings.py
+++ ImagesDB/settings.py
@@ -96,12 +96,13 @@ if 'DATABASE_URL' in os.environ:
 import dj_database_url
 if in_heroku:
     DATABASES = {'default' : dj_database_url.config()}
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.ImagesDB'),
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.ImagesDB'),
+        }
     }
-}
 
 
 # Internationalization
