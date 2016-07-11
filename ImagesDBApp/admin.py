089ee9b5117dc5eae878a17c1488e883747713b5
Leontes <shaljas@correo.ugr.es>
2016-07-11 17:52:09 +0200

    Actualizados Tests. Codigo limpiado

--- ImagesDBApp/admin.py
+++ ImagesDBApp/admin.py
@@ -1,7 +1,5 @@
 #admin.py
 from django.contrib import admin
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
 from .models import Imagen
 
 admin.site.register(Imagen)
--- ImagesDBApp/tests.py
+++ ImagesDBApp/tests.py
@@ -1,52 +1,53 @@
 from django.test import TestCase
+from django import forms
+from .models import Imagen
+from .forms import ImagenForm
+from django.contrib.auth.models import User
 
 # Create your tests here.
-from django import forms
-from .models import Usuario, Imagen
-from .forms import UsuarioForm, ImagenForm
 
 class TestsUsuarios(TestCase):
 	def test_Usuarios(self):
-		user = Usuario(email = 'email', nombre = 'nombre',apellidos = 'apellidos')
+		user = User(username='Usuario1', email='mail@email.com', password='1234')
 		user.save()
 
-		user = Usuario.objects.get(email = 'email')
+		user = User.objects.get(username = 'Usuario1')
 
-		self.assertEqual(user.nombre,'nombre')
+		self.assertEqual(user.username,'Usuario1')
 		print("Se ha creado usuario, Test = OK")
 
 	def test_cambiar_nombre(self):
-		user = Usuario(email = 'email', nombre = 'nombre',apellidos = 'apellidos')
+		user = User(username='Usuario1', email='mail@email.com', password='1234')
 		user.save()
 
-		user.nombre='CambioNombre'
+		user.username='CambioNombre'
 		user.save()
 
-		user = Usuario.objects.get(email = 'email')
+		user = User.objects.get(username = 'CambioNombre')
 
-		self.assertEqual(user.nombre,'CambioNombre')
+		self.assertEqual(user.username,'CambioNombre')
 		print("Se ha realizado el cambio de nombre, Test = OK")
 
 
 	def test_cambiar_email(self):
-		user = Usuario(email = 'email', nombre = 'nombre',apellidos = 'apellidos')
+		user = User(username='Usuario1', email='mail@email.com', password='1234')
 		user.save()
 
-		user.email='CambioEmail'
+		user.email='CambioEmail@email.com'
 		user.save()
 
-		user = Usuario.objects.get(nombre = 'nombre')
+		user = User.objects.get(username = 'Usuario1')
 
-		self.assertEqual(user.email,'CambioEmail')
+		self.assertEqual(user.email,'CambioEmail@email.com')
 		print("Se ha realizado el cambio de email, Test = OK")
 
 
 class TestsImagenes(TestCase):
 	def test_Imagenes(self):
-		user = Usuario(email = 'email', nombre = 'nombre', apellidos = 'apellidos')
+		user = User(username='Usuario1', email='mail@email.com', password='1234')
 		user.save()
 
-		img = Imagen(usuario = user, nombreImg = "nombreImagen",url_img  = 'www.servidordeimagenes/imagen.jpeg')
+		img = Imagen(usuario = user, nombreImg = "nombreImagen", url_img  = 'www.servidordeimagenes/imagen.jpeg')
 		img.save()
 
 		img = Imagen.objects.get(usuario = user)
@@ -56,7 +57,7 @@ class TestsImagenes(TestCase):
 
 
 	def test_cambiar_url_imagen(self):
-		user = Usuario(email = 'email', nombre = 'nombre', apellidos = 'apellidos')
+		user = User(username='Usuario1', email='mail@email.com', password='1234')
 		user.save()
 
 		img = Imagen(usuario = user, nombreImg = "nombreImagen", url_img  = 'www.servidordeimagenes/imagen.jpeg')
@@ -70,14 +71,30 @@ class TestsImagenes(TestCase):
 		self.assertEqual(img.url_img, 'www.servidordeimagenes/imagen.png')
 		print("Se ha modificado la url de una imagen, Test = OK")
 
+	def test_cambiar_nombre_imagen(self):
+		user = User(username='Usuario1', email='mail@email.com', password='1234')
+		user.save()
+
+		img = Imagen(usuario = user, nombreImg = "nombreImagen", url_img  = 'www.servidordeimagenes/imagen.jpeg')
+		img.save()
+
+		img.nombreImg = 'CambioNombreImagen'
+		img.save();
+
+		img = Imagen.objects.get(usuario = user)
+
+		self.assertEqual(img.nombreImg, 'CambioNombreImagen')
+		print("Se ha modificado el nombre de una imagen, Test = OK")
+
+
 	def test_cambiar_usuario_imagen(self):
-		user1 = Usuario(email = 'email1', nombre = 'nombre1', apellidos = 'apellidos1')
+		user1 = User(username='Usuario1', email='mail1@email.com', password='1234')
 		user1.save()
 
 		img = Imagen(usuario = user1, nombreImg = "nombreImagen", url_img  = 'www.servidordeimagenes/imagen.jpeg')
 		img.save()
 
-		user2 = Usuario(email = 'email2', nombre = 'nombre2', apellidos = 'apellidos2')
+		user2 = User(username='Usuario2', email='mail2@email.com', password='1234')
 		user2.save()
 
 		img.usuario = user2
