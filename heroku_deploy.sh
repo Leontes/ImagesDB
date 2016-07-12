874983ee2bdaaf05ed1d5023e8f4d352b5ac5a65
Leontes <shaljas@correo.ugr.es>
2016-07-12 10:47:17 +0200

    Despliegue heroku

--- heroku_deploy.sh
+++ heroku_deploy.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+#Nos bajamos el toolbelt
 wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
+#Nos logeamos en nuestra cuenta de Heroku
 heroku login
 
+#Creamos la app, linkamos el repostorio de Git con ella y la configuramos
 heroku create imagesdb-cc
 heroku git:remote -a imagesdb-cc
 heroku config:set DISABLE_COLLECTSTATIC=1
 
+#Hacemos un push del repostorio
 git add .
 git commit -m "Despliegue heroku"
 git push heroku master
 
+#Preparamos la aplicación para su funcionamiento(asignamos workers, creamos tablas en la DB, etc..)
 heroku ps:scale web=1
 heroku run make light_install
+
+#Lanzamos la aplicación
 heroku open
